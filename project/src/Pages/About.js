357881af2c4d9f269f252f1a98ea49c09f516db3
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-04-04 22:26:45 +0530

    oddOrEven

--- project/src/Pages/About.js
+++ project/src/Pages/About.js
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import '../Counter.css'
+import { useState } from 'react'
 const About = () => {
+  const [num,setNum] = useState('');
+  const handleClick = () =>{
+    const parsedNum = parseInt(num);
+    if(parsedNum%2 === 0){
+      setNum("Even");
+    }
+    else{
+      setNum("Odd");
+    }
+  }
   return (
-    <div>About</div>
+    <div className='middle'>
+      <input placeholder='Enter the number'  onChange={(event) => setNum(event.target.value)} />
+      <button onClick={handleClick}> Odd or Even</button>
+      <h2>The Number is {num}</h2>
+    </div>
+
   )
 }
 
