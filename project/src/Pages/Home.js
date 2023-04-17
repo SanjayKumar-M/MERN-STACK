1d9e463a7a199a44247c0333ffcb362faefb458c
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-04-17 09:15:00 +0530

    todo

--- project/src/Pages/Home.js
+++ project/src/Pages/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Subs from '../Components/Subs'
+import Todo from '../Components/Todo'
 const Home = () => {
   return (
-    <Subs />
+    <>
+      <Subs />
+      <Todo />
+    </>
   )
 }
 
