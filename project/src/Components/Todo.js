ee45494a1c60cfcf22aa1989aabc28fbd277f56b
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-05-04 12:15:19 +0530

    Just changed

--- project/src/Components/Todo.js
+++ project/src/Components/Todo.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todo = () => {
+  const [task, addTask] = useState('');
+  const handleAdd = () =>{
+      addTask = task+1
+  }
   return (
-    <div>Todo</div>
+    <div>
+            Todo List
+
+            <input placeholder='Enter your task'/>
+            <button onClick={handleAdd}>Click Here</button>
+            <table>
+              <tr>{task}</tr>
+            </table>
+    </div>
   )
 }
 
